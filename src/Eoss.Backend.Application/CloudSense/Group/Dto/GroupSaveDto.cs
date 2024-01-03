b89da20575f506dba0556dfd6a02149e93627496
Overwind <overwind@outlook.com>
2024-01-03 18:22:03 +0800

    Temp commit

---
+++ src/Eoss.Backend.Application/CloudSense/Group/Dto/GroupSaveDto.cs
@@ -0,0 +1,11 @@
+﻿using Abp.AutoMapper;
+using Abp.Domain.Entities;
+using System.Data.Common;
+
+namespace Eoss.Backend.CloudSense.Group.Dto
+{
+    [AutoMapTo(typeof(GroupByBehavior))]
+    public class GroupSaveDto : Entity<int>
+    {
+    }
+}
